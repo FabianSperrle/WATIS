6f1c83b268f97fe24286f5ee199b2070a90cb738
leokraemer <leo_urlaub@web.de>
2015-06-29 15:21:35 +0200

    added probably highly useful code snippet

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -8,7 +8,9 @@ using System.Reactive.Linq;
 using Microsoft.ComplexEventProcessing.Extensibility;
 using System.IO;
 using System.Collections.Generic;
+using System.Globalization;
 using LumenWorks.Framework.IO.Csv;
+
 
 namespace StreamInsight21_example_Server
 
@@ -20,6 +22,16 @@ namespace StreamInsight21_example_Server
 {
     class Program
     {
+
+        static IQStreamable<TwitterData> GetTollReadings(this Application app)
+        {
+            return app.DefineEnumerable(() =>
+                // CSV LIBRARY
+                new[] {
+                    PointEvent<TwitterData>.CreateInsert(DateTime.Today, new TwitterData()
+                )}).
+                ToPointStreamable(e => e, AdvanceTimeSettings.IncreasingStartTime);
+        }
         static void Main(string[] args)
         {
             // Create an embedded StreamInsight server
@@ -29,58 +41,9 @@ namespace StreamInsight21_example_Server
                 var host = new ServiceHost(server.CreateManagementService());
                 host.AddServiceEndpoint(typeof(IManagementService), new WSHttpBinding(SecurityMode.Message), "http://localhost:8000/MyStreamInsightServer/Default/");
                 host.Open();
+               
 
-                using (CsvReader csv =
-                new CsvReader(new StreamReader("‪C:\Users\leo\Desktop\day1\2015_02_01_06.csv"), true))
-                {
-                    int fieldCount = csv.FieldCount;
 
-                    string[] headers = csv.GetFieldHeaders();
-                    while (csv.ReadNextRecord())
-                    {
-                        for (int i = 0; i < fieldCount; i++)
-                            Console.Write(string.Format("{0} = {1};",
-                                          headers[i], csv[i]));
-                        Console.WriteLine();
-                    }
-                }
-                /* The following entities will be defined and available in the server for other clients:
-                 * serverApp
-                 * serverSource
-                 * serverSink
-                 * serverProcess
-                 */
-
-                // CREATE a StreamInsight APPLICATION in the server
-                var myApp = server.CreateApplication("serverApp");
-
-                // DEFINE a simple SOURCE (returns a point event every second)
-                var mySource = myApp.DefineObservable(() => Observable.Interval(TimeSpan.FromSeconds(1))).ToPointStreamable(x => PointEvent.CreateInsert(DateTimeOffset.Now, x), AdvanceTimeSettings.StrictlyIncreasingStartTime);
-
-                // DEPLOY the source to the server for clients to use
-                mySource.Deploy("serverSource");
-
-                // Compose a QUERY over the source (return every even-numbered event)
-                var myQuery = from e in mySource
-                              where e % 2 == 0
-                              select e;
-
-                // DEFINE a simple observer SINK (writes the value to the server console)
-                var mySink = myApp.DefineObserver(() => Observer.Create<long>(x => Console.WriteLine("sink_Server..: {0}", x)));
-
-                // DEPLOY the sink to the server for clients to use
-                mySink.Deploy("serverSink");
-
-                // BIND the query to the sink and RUN it
-                using (var proc = myQuery.Bind(mySink).Run("serverProcess"))
-                {
-                    // Wait for the user stops the server
-                    Console.WriteLine("----------------------------------------------------------------");
-                    Console.WriteLine("MyStreamInsightServer is running, press Enter to stop the server");
-                    Console.WriteLine("----------------------------------------------------------------");
-                    Console.WriteLine(" ");
-                    Console.ReadLine();
-                }
                 host.Close();
             }
         }
---
+++ ConsoleApplication1/TwitterData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamInsight21_example_Server
+{
+    class TwitterData
+    {
+        public string TWEET_ID { get; set; }
+        public string TWEET_CREATIONDATE { get; set; }
+        public string TWEET_CONTENT { get; set; }
+        public string TWEET_RETWEETED { get; set; }
+        public string TWEET_RETWEETCOUNT { get; set; }
+        public string TWEET_COORDS { get; set; }
+        public string TWEET_SOURCE { get; set; }
+        public string TWEET_URLS { get; set; }
+        public string MEDIA_URLS { get; set; }
+        public string TWEET_PLACE { get; set; }
+        public string TWEET_REPLYTOSTATUS { get; set; }
+        public string TWEET_REPLYTOUSERID { get; set; }
+        public string TWEET_REPLYTOUSERNAME { get; set; }
+        public string USER_ID { get; set; }
+        public string USER_NAME { get; set; }
+        public string USER_SCREENNAME { get; set; }
+        public string USER_CREATIONDATE { get; set; }
+        public string USER_LANGUAGE { get; set; }
+        public string USER_STATUSESCOUNT { get; set; }
+        public string USER_FOLLOWERSCOUNT { get; set; }
+        public string USER_LOCATION { get; set; }
+        public string USER_DESCRIPTION { get; set; }
+        public string USER_FRIENDSCOUNT { get; set; }
+        public string USER_TIMEZONE { get; set; }
+        public string USER_LISTEDCOUNT { get; set; }
+        public string USER_UTCOFFSET { get; set; }
+    }
+}
