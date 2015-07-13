0398d1d636084d39897b4f6d523d80506b9e4c12
leokraemer <leo_urlaub@web.de>
2015-07-13 16:07:46 +0200

    added fnessecary files

---
+++ ConsoleApplication1/FilterEnglish.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ComplexEventProcessing;
+using Microsoft.ComplexEventProcessing.Extensibility;
+using Microsoft.ComplexEventProcessing.Linq;
+using IvanAkcheurov.NTextCat.Lib;
+using IvanAkcheurov.Commons;
+using IvanAkcheurov.NClassify;
+
+namespace ConsoleApplication1
+{
+    
+    [Serializable]
+    class FilterEnglish : CepPointStreamOperator<TwitterData, TwitterData>
+    {
+        //private RankedLanguageIdentifierFactory factory = new RankedLanguageIdentifierFactory();
+
+        public override bool IsEmpty
+        {
+            get { return false; }
+        }
+        public override IEnumerable<TwitterData> ProcessEvent(PointEvent<TwitterData> e)
+        {
+            
+            //var identifier = factory.Load("Resources\\Core14.profile.xml");
+            //if (identifier.Identify(e.Payload.TWEET_CONTENT).FirstOrDefault().Item1.Iso639_3 == "eng"){
+            if(e.Payload.USER_LANGUAGE.Equals("en")){
+                var returnvalue = new List<TwitterData>();
+                returnvalue.Add(e.Payload);
+                return returnvalue;
+            }
+            return new List<TwitterData>();
+        }
+    }
+}
---
+++ ConsoleApplication1/TweetTextToTerms.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.ComplexEventProcessing;
+using Microsoft.ComplexEventProcessing.Linq;
+using Microsoft.ComplexEventProcessing.Extensibility;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class TweetTextToTerms : CepPointStreamOperator<TwitterData, TwitterDataTerm>
+    {
+
+        public override IEnumerable<TwitterDataTerm> ProcessEvent(PointEvent<TwitterData> inputEvent)
+        {
+            string[] seperators = { " ", ",", ".", "!", "?", ";" };
+            string[] terms = inputEvent.Payload.TWEET_CONTENT.Split(seperators, StringSplitOptions.RemoveEmptyEntries);
+            return terms.Select(e => new TwitterDataTerm(e, inputEvent.Payload));
+        }
+
+        public override bool IsEmpty
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
